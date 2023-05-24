16758213f4010921d4b0bc97cfc5855eefc7e0eb
SEWOONG LEE <dltpdnd1234@gmail.com>
2023-05-23 23:15:16 -0400

    typescript setting

--- Cloudy/src/controller/routes/loginRoutes.js
+++ Cloudy/src/controller/routes/loginRoutes.js
@@ -1,71 +1,76 @@
-const express = require('express');
-const {User, seedUser, addUser} = require("../../model/userModel");
-const session = require("express-session");
-const router = express.Router();
-var client;
+const express = require("express")
+const { User, seedUser, addUser } = require("../../model/userModel")
+const session = require("express-session")
+const router = express.Router()
+var client
 
-const parser = express.urlencoded({extended: false});
+const parser = express.urlencoded({ extended: false })
 const userSession = session({
-    name: "session",
-    secret: "secrets",
-});
+  name: "session",
+  secret: "secrets",
+})
 
-//Middleware
-router.use((req,res,next) =>{
-    req.model = User;
-    next();
-});
+router.use((req, res, next) => {
+  req.model = User
+  next()
+})
 
 //Login Routes
-router.route("/")
-    .get((req,res) =>{
-        res.render("login.njk");
-    })
-    .post(parser, userSession, async (req,res) =>{
-    req.model.authenticate(req.body.username, req.body.password, function(user) {
+router
+  .route("/")
+  .get((req, res) => {
+    res.render("login.njk")
+  })
+  .post(parser, userSession, async (req, res) => {
+    req.model.authenticate(
+      req.body.username,
+      req.body.password,
+      function (user) {
         if (user) {
-            console.log("Logged in", user);
-            req.session.regenerate( function(err) {
-                req.session.user = user;
-                res.redirect("/files");
-            });
+          console.log("Logged in", user)
+          req.session.regenerate(function (err) {
+            req.session.user = user
+            res.redirect("/files")
+          })
+        } else {
+          res.redirect("/")
+          res.send("Incorrect username or password")
         }
-        else{
-            res.redirect("/");
-            res.send("Incorrect username or password");
-        }
-    })
-});
+      }
+    )
+  })
 
 //Registration Routes
-router.route("/register")
-    .get((req,res) =>{
-        res.render("register.njk");
+router
+  .route("/register")
+  .get((req, res) => {
+    res.render("register.njk")
+  })
+  .post(parser, async (req, res) => {
+    req.model.checkDuplicate(req.body.username, function (user) {
+      if (user) {
+        res.send("Username taken!")
+      } else {
+        console.log(req.body)
+        addUser(req.body.username, req.body.password)
+        setTimeout(() => {
+          res.redirect("/")
+        }, 2000)
+      }
     })
-    .post(parser, async(req,res) =>{
-        req.model.checkDuplicate(req.body.username, function(user) {
-            if (user) {
-                res.send("Username taken!");
-            }
-            else{
-                console.log(req.body);
-                addUser(req.body.username, req.body.password);
-                setTimeout(() =>{
-                    res.redirect("/");
-                }, 2000);
-            }
-        })
-    });
+  })
 
-    seedUser(client);
+seedUser(client)
 
 //Logout (needs to be implemented on a logout button)
-router.route("/logout").get((req,res) =>{
-    req.session.destroy( () => {res.redirect("/")});
-});
+router.route("/logout").get((req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/")
+  })
+})
 
-function configure(mongoClient){
-    client = mongoClient;
-    return router;
+function configure(mongoClient) {
+  client = mongoClient
+  return router
 }
-module.exports = configure;
+module.exports = configure
